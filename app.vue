6e546caefb308159b4b7d0325f218ab35f9f653d
Bernhard Berger <beb@mmcagentur.at>
2024-07-05 14:46:20 +0200

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+	<NuxtImg src="/tiger.jpg" format="webp" style="max-width: 100%; height: auto;" :width="640" :height="480" />	
+	{{ time }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const time = await useFetch('https://worldtimeapi.org/api/timezone/Europe/Vienna')
+</script>
